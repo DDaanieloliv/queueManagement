623d971819bbe97ea67b231693c69dba8f26f888
DDaanieloliv <126197002+DDaanieloliv@users.noreply.github.com>
2024-09-09 10:12:17 -0300

    Create Remedios.java

---
+++ src/main/java/ddaaniel/io/queueManagement/model/Remedios.java
@@ -0,0 +1,16 @@
+package ddaaniel.io.queueManagement.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.*;
+
+@Data
+@Entity
+public class {
+
+  @Id 
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  
+}
